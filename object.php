b593cefdd716bc74ff30ecdc9253de1a96435ef2
Jaydeep K <jaydeepk@websoptimization.com>
2019-09-04 16:58:33 +0530

    object

--- object.php
+++ object.php
@@ -16,9 +16,26 @@ class TV{
 	}
 }
 
-$tv_one = new TV;
+$tv_one = new TV; // object of the class
 $tv_two = new TV;
 
+
+//for tv_one
 $tv_one->volumeUp();
 
-echo $tv_one->volume;
+echo "For Tv One:-<br>";
+
+echo "Volume:-".$tv_one->volume."<br>";
+
+echo "Model:-".$tv_one->model."<br>";
+
+echo "<br><br><br>";
+
+//for tv_two
+$tv_two->volumeUp();
+
+echo "For Tv Two:-<br>";
+
+echo "Volume:-".$tv_two->volume."<br>";
+
+echo "Model:-".$tv_two->model = "string";
